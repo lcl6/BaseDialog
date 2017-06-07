10a2413c37c8be204e71c6dcee3094cd003fcbaa
chenlindaydayup <754361440@qq.com>
2017-06-07 10:00:56 +0800

    Initial commit

---
+++ app/src/main/java/com/lcl6/cn/basedialog/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lcl6.cn.basedialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
