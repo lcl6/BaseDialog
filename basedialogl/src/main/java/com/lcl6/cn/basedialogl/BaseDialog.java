761ebef0bf035839fca30564cbcd6ddd55511f85
chenlindaydayup <754361440@qq.com>
2017-06-07 10:25:42 +0800

    添加库

---
+++ basedialogl/src/main/java/com/lcl6/cn/basedialogl/BaseDialog.java
@@ -0,0 +1,61 @@
+package com.lcl6.cn.basedialogl;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StyleRes;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+/**
+ * 可扩展基类
+ * Created by liancl on 2017/6/7.
+ */
+
+public abstract class BaseDialog extends Dialog {
+    public BaseDialog(@NonNull Context context) {
+        super(context,R.style.BaseDialog);
+    }
+
+    public BaseDialog(@NonNull Context context, @StyleRes int themeResId) {
+        super(context, themeResId);
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(layoutResID);
+        initParams(getWindow());
+    }
+
+    protected abstract void initParams(Window window);
+
+    @Override
+    public void setContentView(@NonNull View view) {
+        super.setContentView(view);
+        initParams(getWindow());
+    }
+
+    @Override
+    public void setContentView(@NonNull View view, @Nullable ViewGroup.LayoutParams params) {
+        super.setContentView(view, params);
+        initParams(getWindow());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getlayoutId());
+        initData();
+    }
+
+
+
+    /** 初始化数据 */
+    protected abstract void initData();
+    /** 初始化布局  */
+    protected abstract int getlayoutId();
+}
