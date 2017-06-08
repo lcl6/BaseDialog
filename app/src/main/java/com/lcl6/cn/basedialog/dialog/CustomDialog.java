0b1f84515d4fd24fbf41244496c079b520d78edf
chenlindaydayup <754361440@qq.com>
2017-06-08 13:47:12 +0800

    添加库

---
+++ app/src/main/java/com/lcl6/cn/basedialog/dialog/CustomDialog.java
@@ -0,0 +1,33 @@
+package com.lcl6.cn.basedialog.dialog;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.lcl6.cn.basedialogl.BaseDialog;
+
+/**
+ * Created by liancl on 2017/6/8.
+ */
+
+public class CustomDialog extends BaseDialog {
+    public CustomDialog(@NonNull Context context) {
+        super(context);
+    }
+
+
+    @Override
+    protected void findView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getlayoutId() {
+        return 0;
+    }
+}
