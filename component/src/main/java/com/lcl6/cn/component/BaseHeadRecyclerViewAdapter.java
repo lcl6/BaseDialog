155c0418642233378a2a0a19789e8c76520a793d
chenlindaydayup <754361440@qq.com>
2017-08-02 10:59:50 +0800

    修改sdk版本号 添加compinent

---
+++ component/src/main/java/com/lcl6/cn/component/BaseHeadRecyclerViewAdapter.java
@@ -0,0 +1,130 @@
+package com.lcl6.cn.component;
+
+import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by liancl on 2017/8/2.
+ */
+
+public abstract class BaseHeadRecyclerViewAdapter<H, T> extends BaseRecyclerViewAdapter<T>{
+
+    /** 头部 */
+    private static final int VIEW_TYPE_HEAD = 0;
+    /** 数据列表 */
+    private static final int VIEW_TYPE_ITEM = 1;
+
+    /** 头信息数据 */
+    private H mHeadData;
+
+    public BaseHeadRecyclerViewAdapter(Context context) {
+        super(context);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0){
+            return VIEW_TYPE_HEAD;
+        }
+        return VIEW_TYPE_ITEM;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_HEAD){
+            return getHeadViewHolder(parent);
+        }
+        return getItemViewHolder(parent);
+    }
+
+    /** 获取头布局的ViewHolder */
+    protected abstract RecyclerView.ViewHolder getHeadViewHolder(ViewGroup parent);
+    /** 获取列表布局的ViewHolder */
+    protected abstract RecyclerView.ViewHolder getItemViewHolder(ViewGroup parent);
+
+    /**
+     * 设置头信息数据
+     * @param headData 头信息数据
+     */
+    public void setHeadData(H headData){
+        mHeadData = headData;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHeadData != null ? super.getItemCount() + 1 : super.getItemCount();
+    }
+
+    /** 获取头信息数据 */
+    public H getHeadData(){
+        return mHeadData;
+    }
+
+    /**
+     * 通过正常的position获取带有Head的List数据
+     * @param position 0开始的位置
+     */
+    protected T getItemUnderHead(int position) {
+        return getItem(position - 1);
+    }
+
+    @Override
+    protected void setItemClick(final RecyclerView.ViewHolder holder, final int position) {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(position > 0 && mOnItemClickListener != null) {
+                    mOnItemClickListener.onItemClick(holder, getItemUnderHead(position), position - 1);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void setItemLongClick(final RecyclerView.ViewHolder holder, final int position) {
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (position > 0 && mOnItemLongClickListener != null){
+                    mOnItemLongClickListener.onItemLongClick(holder, getItemUnderHead(position), position - 1);
+                }
+                return false;
+            }
+        });
+    }
+    /**网格布局处理方法*/
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        RecyclerView.LayoutManager manager = recyclerView.getLayoutManager();
+        if(manager instanceof GridLayoutManager) {// 网格布局时要优化加载排版
+            final GridLayoutManager layoutManager = ((GridLayoutManager) manager);
+            if (layoutManager.getOrientation() == GridLayoutManager.HORIZONTAL){//横向排版不处理
+                return;
+            }
+            layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    return position == 0 ? layoutManager.getSpanCount() : 1;
+                }
+            });
+        }
+    }
+
+    /**瀑布流处理方法*/
+    @Override
+    public void onViewAttachedToWindow(RecyclerView.ViewHolder holder) {
+        super.onViewAttachedToWindow(holder);
+        ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+        if(lp != null
+                && lp instanceof StaggeredGridLayoutManager.LayoutParams
+                && holder.getLayoutPosition() == 0) {
+            StaggeredGridLayoutManager.LayoutParams p = (StaggeredGridLayoutManager.LayoutParams) lp;
+            p.setFullSpan(true);
+        }
+    }
+}
---
+++ component/src/main/java/com/lcl6/cn/component/BaseRecyclerViewAdapter.java
@@ -0,0 +1,143 @@
+package com.lcl6.cn.component;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by liancl on 2017/8/2.
+ */
+
+public abstract class BaseRecyclerViewAdapter<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    /** 数据列表 */
+    private List<T> mData;
+    /** item点击 */
+    protected OnItemClickListener<T> mOnItemClickListener;
+    /** item长按 */
+    protected OnItemLongClickListener<T> mOnItemLongClickListener;
+
+    private Context mContext;
+
+    public BaseRecyclerViewAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    public Context getContext(){
+        return mContext;
+    }
+
+    /**
+     * 获取数据
+     * @param position 位置
+     */
+    public T getItem(int position) {
+        if (mData == null || mData.size() == 0){
+            return null;
+        }
+        return mData.get(position);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        setItemClick(holder, position);
+        setItemLongClick(holder, position);
+        onBind(holder, position);
+    }
+
+    /** 设置点击事件 */
+    protected void setItemClick(final RecyclerView.ViewHolder holder, final int position) {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(position >= 0 && mOnItemClickListener != null) {
+                    mOnItemClickListener.onItemClick(holder, getItem(position), position);
+                }
+            }
+        });
+    }
+
+    /** 设置长按事件 */
+    protected void setItemLongClick(final RecyclerView.ViewHolder holder, final int position) {
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (position >= 0 && mOnItemLongClickListener != null){
+                    mOnItemLongClickListener.onItemLongClick(holder, getItem(position), position);
+                }
+                return false;
+            }
+        });
+    }
+
+    protected abstract void onBind(RecyclerView.ViewHolder holder, int position);
+
+    @Override
+    public int getItemCount() {
+        if (mData == null){
+            return 0;
+        }
+        return mData.size();
+    }
+
+    /**
+     * 设置数据
+     * @param data 数据列表
+     */
+    public void setData(List<T> data){
+        this.mData = data;
+    }
+
+    /** 在onCreateViewHolder方法中根据layoutId获取View */
+    protected View getLayoutView(ViewGroup parent, int layoutId){
+        return LayoutInflater.from(mContext).inflate(layoutId, parent, false);
+    }
+
+    /**
+     * 设置itemview的宽度
+     * @param itemView holder的itemview
+     * @param width 宽度
+     */
+    protected void setItemViewWidth(View itemView, int width){
+        ViewGroup.LayoutParams layoutParams = itemView.getLayoutParams();
+        layoutParams.width = width;
+        itemView.setLayoutParams(layoutParams);
+    }
+
+    /**
+     * 带动画的删除item并刷新数据
+     * @param position 位置
+     */
+    public void notifyItemRemovedChanged(int position){
+        if (getItemCount() == 0){
+            return;
+        }
+        mData.remove(position);
+        notifyItemRemoved(position);
+        if (position != mData.size()) { // 如果移除的是最后一个，忽略
+            //对于被删掉的位置及其后range大小范围内的view进行重新onBindViewHolder
+            notifyItemRangeChanged(position, mData.size() - position);
+        }
+    }
+
+    /** 设置点击事件监听器 */
+    public void setOnItemClickListener(OnItemClickListener<T> listener){
+        mOnItemClickListener = listener;
+    }
+    /** 设置长按事件监听器 */
+    public void setOnItemLongClickListener(OnItemLongClickListener<T> listener){
+        mOnItemLongClickListener = listener;
+    }
+
+    public interface OnItemClickListener<T> {
+        void onItemClick(RecyclerView.ViewHolder viewHolder, T item, int position);
+    }
+
+    public interface OnItemLongClickListener<T> {
+        void onItemLongClick(RecyclerView.ViewHolder viewHolder, T item, int position);
+    }
+}
