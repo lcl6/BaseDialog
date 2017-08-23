81bb9f63e44cf6f834a0499244bcc26b464b195d
chenlindaydayup <754361440@qq.com>
2017-08-23 13:53:05 +0800

    添加基础的mvp

--- app/src/main/java/com/lcl6/cn/basedialog/MainActivity.java
+++ app/src/main/java/com/lcl6/cn/basedialog/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @OnClick({R.id.tv_cumston,R.id.tv_bottom,R.id.tv_right,R.id.tv_left,R.id.tv_next,R.id.tv_jsoup,R.id.tv_mvp，R.id.tv_widget})
+    @OnClick({R.id.tv_cumston,R.id.tv_bottom,R.id.tv_right,R.id.tv_left,R.id.tv_next,R.id.tv_jsoup,R.id.tv_mvp,R.id.tv_widget})
     public void onClick(View v){
         switch (v.getId()){
             case R.id.tv_cumston:
