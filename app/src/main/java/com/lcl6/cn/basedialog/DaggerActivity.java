91f30045f2036de20f09cdec85afc127d2b15925
chenlindaydayup <754361440@qq.com>
2017-08-30 10:59:31 +0800

    添加沉浸状态栏的工具类

--- app/src/main/java/com/lcl6/cn/basedialog/DaggerActivity.java
+++ app/src/main/java/com/lcl6/cn/basedialog/DaggerActivity.java
@@ -3,6 +3,7 @@ package com.lcl6.cn.basedialog;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.view.View;
 
 import com.lcl6.cn.basedialog.constant.Constant;
 import com.lcl6.cn.basedialog.di.bean.ClassA;
@@ -11,8 +12,12 @@ import com.lcl6.cn.basedialog.di.component.DaggerClassAComponent;
 import com.lcl6.cn.basedialog.di.model.ModuleA;
 import com.lcl6.cn.component.base.activity.BaseActivity;
 import com.lcl6.cn.component.base.mvp.presnenter.RxPresenter;
+import com.lcl6.cn.utils.ToastUtils;
+import com.lcl6.cn.utils.statusbar.QMUIStatusBarHelper;
 
 import javax.inject.Inject;
+
+import butterknife.OnClick;
 
 /**
  * Created by liancl on 2017/8/29.
@@ -62,6 +67,22 @@ public class DaggerActivity extends BaseActivity {
         Log.e(Constant.TAG, "initData:a2 "+a2 );
         Log.e(Constant.TAG, "initData:b2"+b1);
 
+    }
+
+    @OnClick({R.id.tv_status,R.id.tv_status_white,R.id.tv_status_height})
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.tv_status:
+                QMUIStatusBarHelper.setStatusBarLightMode(this);
+                break;
+            case R.id.tv_status_white:
+                QMUIStatusBarHelper.setStatusBarDarkMode(this);
+                break;
+            case R.id.tv_status_height:
+                int statusbarHeight = QMUIStatusBarHelper.getStatusbarHeight(this);
+                ToastUtils.showShort("状态栏的高度为："+statusbarHeight);
+                break;
+        }
     }
 
 
