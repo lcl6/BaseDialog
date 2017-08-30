8e25434a97e1138db98c98598667275009634ca8
chenlindaydayup <754361440@qq.com>
2017-08-30 11:29:39 +0800

    添加两个动画

--- app/src/main/java/com/lcl6/cn/basedialog/DaggerActivity.java
+++ app/src/main/java/com/lcl6/cn/basedialog/DaggerActivity.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.TextView;
 
 import com.lcl6.cn.basedialog.constant.Constant;
 import com.lcl6.cn.basedialog.di.bean.ClassA;
@@ -12,11 +15,14 @@ import com.lcl6.cn.basedialog.di.component.DaggerClassAComponent;
 import com.lcl6.cn.basedialog.di.model.ModuleA;
 import com.lcl6.cn.component.base.activity.BaseActivity;
 import com.lcl6.cn.component.base.mvp.presnenter.RxPresenter;
+import com.lcl6.cn.component.base.toplayout.QMUIDirection;
+import com.lcl6.cn.component.base.toplayout.QMUIViewHelper;
 import com.lcl6.cn.utils.ToastUtils;
 import com.lcl6.cn.utils.statusbar.QMUIStatusBarHelper;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 /**
@@ -24,13 +30,13 @@ import butterknife.OnClick;
  */
 
 public class DaggerActivity extends BaseActivity {
-
-
     @Inject
     ClassA classA;
-
     @Inject
     ClassB classB;
+    @BindView(R.id.tv_float)
+    TextView mFloatView;
+
 
     public static void start(Context context) {
         Intent starter = new Intent(context, DaggerActivity.class);
@@ -69,7 +75,7 @@ public class DaggerActivity extends BaseActivity {
 
     }
 
-    @OnClick({R.id.tv_status,R.id.tv_status_white,R.id.tv_status_height})
+    @OnClick({R.id.tv_status,R.id.tv_status_white,R.id.tv_status_height,R.id.tv_fade_in, R.id.tv_slide_in})
     public void onClick(View v){
         switch (v.getId()){
             case R.id.tv_status:
@@ -82,6 +88,19 @@ public class DaggerActivity extends BaseActivity {
                 int statusbarHeight = QMUIStatusBarHelper.getStatusbarHeight(this);
                 ToastUtils.showShort("状态栏的高度为："+statusbarHeight);
                 break;
+            case R.id.tv_fade_in:
+                mFloatView.clearAnimation();
+                AlphaAnimation alphaAnimation = mFloatView.getVisibility() == View.GONE ? QMUIViewHelper.fadeIn(mFloatView, 500, null, true) : QMUIViewHelper.fadeOut(mFloatView, 500, null, true);
+                break;
+            case R.id.tv_slide_in:
+                mFloatView.clearAnimation();
+                TranslateAnimation translateAnimation = mFloatView.getVisibility() == View.GONE ? QMUIViewHelper.slideIn(mFloatView, 500, null, true, QMUIDirection.BOTTOM_TO_TOP) : QMUIViewHelper.slideOut(mFloatView, 500, null, true, QMUIDirection.TOP_TO_BOTTOM);
+                break;
+
+
+
+
+
         }
     }
 
