1a24d5c5362cea09001492567fcdb640d8211693
chenlindaydayup <754361440@qq.com>
2017-08-31 10:42:58 +0800

    添加测试

--- app/src/main/java/com/lcl6/cn/basedialog/DaggerScopActivity.java
+++ app/src/main/java/com/lcl6/cn/basedialog/DaggerScopActivity.java
@@ -2,15 +2,25 @@ package com.lcl6.cn.basedialog;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.lcl6.cn.basedialog.adapter.EdtextAdapter;
 import com.lcl6.cn.basedialog.app.App;
 import com.lcl6.cn.basedialog.di.bean.MyClassB;
 import com.lcl6.cn.basedialog.di.component.DaggerActivityComponent;
 import com.lcl6.cn.basedialog.di.model.ActivityModule;
+import com.lcl6.cn.component.adapter.BaseRecyclerViewAdapter;
 import com.lcl6.cn.component.base.activity.BaseActivity;
 import com.lcl6.cn.component.base.mvp.presnenter.RxPresenter;
+import com.lcl6.cn.utils.ToastUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
+
+import butterknife.BindView;
 
 /**
  * Created by liancl on 2017/8/30.
@@ -20,6 +30,13 @@ public class DaggerScopActivity extends BaseActivity {
 
     @Inject
     MyClassB classB;
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+    EdtextAdapter mAdapter;
+
+    List<String> mlist;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, DaggerScopActivity.class);
@@ -32,19 +49,44 @@ public class DaggerScopActivity extends BaseActivity {
 
     @Override
     protected int setLayoutId() {
-        return R.layout.activity_dagger;
+        return R.layout.activity_dagger_list;
     }
 
     @Override
     protected void initView() {
 
+        mlist=new ArrayList<>();
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false));
+        mAdapter = new EdtextAdapter(getContext());
+        mAdapter.setData(mlist);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
     protected void initData() {
         DaggerActivityComponent.builder().activityModule(new ActivityModule(2)).appComponent(App.getAppComponent()).build().inject(this);
+        for (int i = 0; i < 20; i++) {
+            mlist.add("你好");
+        }
+        mAdapter.notifyDataSetChanged();
     }
 
+    @Override
+    protected void initViewListener() {
+        super.initViewListener();
+        if(mAdapter!=null){
+            mAdapter.setOnItemClickListener(new BaseRecyclerViewAdapter.OnItemClickListener<String>() {
+                @Override
+                public void onItemClick(RecyclerView.ViewHolder viewHolder, String item, int position) {
+                    ToastUtils.showShort("点击了"+position);
+                }
+            });
 
-
+        }
+    }
 }
---
+++ app/src/main/java/com/lcl6/cn/basedialog/adapter/EdtextAdapter.java
@@ -0,0 +1,52 @@
+package com.lcl6.cn.basedialog.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.lcl6.cn.basedialog.R;
+import com.lcl6.cn.component.adapter.BaseRecyclerViewAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by liancl on 2017/8/31.
+ */
+
+public class EdtextAdapter extends BaseRecyclerViewAdapter<String> {
+
+    public EdtextAdapter(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onBind(RecyclerView.ViewHolder holder, final int position) {
+
+        ((EditextHolder)holder).input.setText(position+"");
+        ((EditextHolder)holder).textViewposition.setText("测试点击事件"+position);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new EditextHolder(getLayoutView(parent, R.layout.item_editext));
+    }
+    class EditextHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.ed_intput)
+        EditText input;
+        @BindView(R.id.tv_pos)
+        TextView textViewposition;
+        @BindView(R.id.lin_item)
+        LinearLayout linitem;
+
+        public EditextHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+
+        }
+    }
+}
