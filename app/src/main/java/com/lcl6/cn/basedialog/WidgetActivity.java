735b2f236645af3147ffeb540f53ae918d3bd698
chenlindaydayup <754361440@qq.com>
2017-09-05 18:01:35 +0800

    增加返回关闭的方法

--- app/src/main/java/com/lcl6/cn/basedialog/WidgetActivity.java
+++ app/src/main/java/com/lcl6/cn/basedialog/WidgetActivity.java
@@ -1,23 +1,133 @@
 package com.lcl6.cn.basedialog;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.lcl6.cn.component.base.activity.BaseActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
 
 /**
  * Created by liancl on 2017/8/21
  */
 
-public class WidgetActivity extends Activity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_widget);
-    }
+public class WidgetActivity extends BaseActivity {
+
+    @BindView(R.id.ed_search)
+    EditText mEdSearch;
+    PublishSubject<String> mPublishSubject;
+    @BindView(R.id.tv_result)
+    TextView tvResult;
+
     public static void start(Context context) {
         Intent starter = new Intent(context, WidgetActivity.class);
         context.startActivity(starter);
+    }
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_widget;
+    }
+
+    @Override
+    protected void initView() {
+        mEdSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.length() <= 0) {
+                    return;
+                }
+            }
+        });
+
+        mPublishSubject = PublishSubject.create();
+
+        mPublishSubject.debounce(200, TimeUnit.SECONDS).filter(new Predicate<String>() {
+            @Override
+            public boolean test(@NonNull String s) throws Exception {
+                return s.length() > 0;
+            }
+        }).switchMap(new Function<String, ObservableSource<?>>() {
+            @Override
+            public ObservableSource<?> apply(@NonNull String query) throws Exception {
+                return getSearchObervable(query);
+            }
+        }).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Object>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull Object o) {
+                        String o1 = (String) o;
+                        tvResult.setText(o1);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    private ObservableSource<?> getSearchObervable(final String query) {
+        return Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> observableEmitter) throws Exception {
+                Log.d("SearchActivity", "开始请求，关键词为：" + query);
+                try {
+                    Thread.sleep(100 + (long) (Math.random() * 500));
+                } catch (InterruptedException e) {
+                    if (!observableEmitter.isDisposed()) {
+                        observableEmitter.onError(e);
+                    }
+                }
+                Log.d("SearchActivity", "结束请求，关键词为：" + query);
+                observableEmitter.onNext("完成搜索，关键词为：" + query);
+                observableEmitter.onComplete();
+
+            }
+        }).subscribeOn(Schedulers.io());
+    }
+
+    @Override
+    protected void initData() {
+
     }
 }
