ce6da46c6a208b240ebec6a282bdcd2d4334ff6c
chenlindaydayup <754361440@qq.com>
2017-09-06 10:20:15 +0800

    rxjava

--- app/src/main/java/com/lcl6/cn/basedialog/WidgetActivity.java
+++ app/src/main/java/com/lcl6/cn/basedialog/WidgetActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.lcl6.cn.basedialog.constant.Constant;
 import com.lcl6.cn.component.base.activity.BaseActivity;
 
 import java.util.concurrent.TimeUnit;
@@ -17,12 +18,12 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
 
@@ -37,6 +38,7 @@ public class WidgetActivity extends BaseActivity {
     PublishSubject<String> mPublishSubject;
     @BindView(R.id.tv_result)
     TextView tvResult;
+    private CompositeDisposable mCompositeDisposable;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, WidgetActivity.class);
@@ -50,6 +52,8 @@ public class WidgetActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        onLoadSuccessStatus();
+
         mEdSearch.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -66,37 +70,31 @@ public class WidgetActivity extends BaseActivity {
                 if (editable.length() <= 0) {
                     return;
                 }
+                startSearch(editable.toString());
             }
         });
-
         mPublishSubject = PublishSubject.create();
-
-        mPublishSubject.debounce(200, TimeUnit.SECONDS).filter(new Predicate<String>() {
+        mPublishSubject.debounce(200, TimeUnit.MILLISECONDS).filter(new Predicate<String>() {
             @Override
             public boolean test(@NonNull String s) throws Exception {
                 return s.length() > 0;
             }
-        }).switchMap(new Function<String, ObservableSource<?>>() {
+        }).switchMap(new Function<String, ObservableSource<String>>() {
             @Override
-            public ObservableSource<?> apply(@NonNull String query) throws Exception {
+            public ObservableSource<String> apply(@NonNull String query) throws Exception {
                 return getSearchObervable(query);
             }
         }).observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<Object>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
+                .subscribe(new DisposableObserver<String>() {
 
+                    @Override
+                    public void onNext(String s) {
+                        tvResult.setText(s);
                     }
 
                     @Override
-                    public void onNext(@NonNull Object o) {
-                        String o1 = (String) o;
-                        tvResult.setText(o1);
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-
+                    public void onError(Throwable throwable) {
+                        Log.e(Constant.TAG, "onError: ");
                     }
 
                     @Override
@@ -104,9 +102,15 @@ public class WidgetActivity extends BaseActivity {
 
                     }
                 });
+        mCompositeDisposable = new CompositeDisposable();
+        mCompositeDisposable.add(mCompositeDisposable);
     }
 
-    private ObservableSource<?> getSearchObervable(final String query) {
+    private void startSearch(String query) {
+        mPublishSubject.onNext(query);
+    }
+
+    private ObservableSource<String> getSearchObervable(final String query) {
         return Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<String> observableEmitter) throws Exception {
@@ -128,6 +132,15 @@ public class WidgetActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mCompositeDisposable != null) {
+            mCompositeDisposable.clear();
+        }
 
     }
 }
