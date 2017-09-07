ff7efd1aec156319a80543c8ac8a395a1a77ea9e
chenlindaydayup <754361440@qq.com>
2017-09-07 17:30:07 +0800

    学习子弟内衣

--- app/src/main/java/com/lcl6/cn/basedialog/widget/CircleText.java
+++ app/src/main/java/com/lcl6/cn/basedialog/widget/CircleText.java
@@ -1,11 +1,14 @@
 package com.lcl6.cn.basedialog.widget;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.widget.TextView;
+
+import com.lcl6.cn.basedialog.R;
 
 /**
  * Created by liancl on 2017/8/21
@@ -27,7 +30,15 @@ public class CircleText extends android.support.v7.widget.AppCompatTextView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+//        Paint paint = new Paint();
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setColor(0xffff00ff);
+//        paint.setStrokeWidth(10);
+//        paint.setAntiAlias(true);
+//        canvas.drawCircle(100,100,80,paint);
+
         Paint paint = new Paint();
-        canvas.drawCircle(100,100,20,paint);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        canvas.drawBitmap(bitmap,0,0,paint);
     }
 }
