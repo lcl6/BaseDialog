30feb6db76ce6fe304d9c40c7be90e767aadc058
chenlindaydayup <754361440@qq.com>
2017-09-08 11:19:59 +0800

    添加自定义控件

--- app/src/main/java/com/lcl6/cn/basedialog/widget/CircleText.java
+++ app/src/main/java/com/lcl6/cn/basedialog/widget/CircleText.java
@@ -1,14 +1,11 @@
 package com.lcl6.cn.basedialog.widget;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-
-import com.lcl6.cn.basedialog.R;
 
 /**
  * Created by liancl on 2017/8/21
@@ -30,6 +27,9 @@ public class CircleText extends android.support.v7.widget.AppCompatTextView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        Paint paint = new Paint();
+
+        //自定义员
 //        Paint paint = new Paint();
 //        paint.setStyle(Paint.Style.STROKE);
 //        paint.setColor(0xffff00ff);
@@ -37,8 +37,61 @@ public class CircleText extends android.support.v7.widget.AppCompatTextView {
 //        paint.setAntiAlias(true);
 //        canvas.drawCircle(100,100,80,paint);
 
-        Paint paint = new Paint();
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
-        canvas.drawBitmap(bitmap,0,0,paint);
+        //自定义bitmap
+//        Paint paint = new Paint();
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+//        canvas.drawBitmap(bitmap,0,0,paint);
+
+        //自定义矩形 并设置颜色
+        paint.setColor((Color.parseColor("#009688")));
+
+
+        //第一种写法
+//        canvas.drawRect(30,30,100,200,paint);
+//        第二种写法
+//        RectF rect = new RectF();
+//        rect.left=30;
+//        rect.top=30;
+//        rect.bottom=300;
+//        rect.right=400;
+//        canvas.drawRect(rect,paint);
+        // 带圆角的矩形
+//        canvas.drawRoundRect(rect,10,10,paint);
+//       // 带圆角的矩形
+//        canvas.drawRoundRect(30,30,300,300,10,10,paint);
+//        paint.setColor((Color.parseColor("#129688")));
+//        paint.setStrokeWidth(20);
+//        canvas.drawLine(250,250,400,0,paint);
+//
+//        Paint paint2 = new Paint();
+//        paint2.setColor((Color.parseColor("#129688")));
+//        paint2.setStrokeWidth(20);
+//        canvas.drawLine(400,0,650,250,paint2);
+
+        //线性渐变
+        //可以有多种位置
+//        LinearGradient linearGradient = new LinearGradient(0, 0, 200, 200, new int[]{Color.BLACK, Color.GREEN, Color.BLUE}, null, Shader.TileMode.CLAMP);
+////        LinearGradient linearGradient = new LinearGradient(0, 0, 200, 200, Color.parseColor("#E91E63"), Color.parseColor("#2196F3"), Shader.TileMode.MIRROR );
+//        paint.setShader(linearGradient);
+//        canvas.drawRect(0,0,200,200,paint);
+
+        //辐射渐变
+//        RadialGradient radialGradient = new RadialGradient(100, 100, 80, Color.RED,  Color.BLUE, Shader.TileMode.CLAMP);
+//        paint.setShader(radialGradient);
+//        canvas.drawCircle(100,100,100,paint);
+
+        //扫描渐变
+//        SweepGradient sweepGradient = new SweepGradient(100, 100, Color.parseColor("#E91E63"), Color.parseColor("#2196F3"));
+//        paint.setShader(sweepGradient);
+//        canvas.drawCircle(100,100,100,paint);
+
+        //bitmap 着色
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.aaa);
+//        BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.MIRROR, Shader.TileMode.MIRROR);//CLAMP  拉伸  REPEAT 重复  MIRROR  镜像
+//        paint.setShader(bitmapShader);
+//        canvas.drawRoundRect(10,10,300,300,10,10,paint);
+//        canvas.drawCircle(100,100,100,paint);
+
+
     }
 }
